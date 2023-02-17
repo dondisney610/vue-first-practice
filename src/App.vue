bb360ff44280ddaa94315771ffb26c95ce63b65b
Riku <rikudonoyamaguchi@gmail.com>
2023-02-17 23:58:19 +0900

    add ClockScreen component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Clock />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Clock from "./components/ClockScreen.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Clock,
+  },
+};
 </script>
 
 <style>
